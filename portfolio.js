ca2fac8cd6bb905842d1a1aacaf1f7f2b5e41a1b
MacWeTT <97174936+MacWeTT@users.noreply.github.com>
2022-01-26 16:24:10 +0530

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,4 +1,18 @@
-function menutoggle(){
-    const nav = document.querySelector("nav")
-    nav.classList.toggle("clicked-nav")
+const checkbox = 
+document.getElementById('checkbox');
+
+checkbox.addEventListener('change', () =>{
+	document.body.classList.toggle('dark');
+});
+
+function alertmsg(){
+	var txt;
+  if (confirm("Do you want to submit your feedback?")) {
+    txt = "Thank you for your feedback!";
+  } else {
+    txt = "Your response has not been submitted.";
+  }
+  document.getElementById("feedbackresult").innerHTML = txt;
 }
+
+
